68f279d7105ebe04892769d523556242b506bed3
Ricardo <ricardo_lda@hotmail.com>
2019-08-13 22:49:05 +0000

    removido venv do git

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Desenvolvimento\Python\Django\API-REST Framework Django\pontos_turisticos\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
